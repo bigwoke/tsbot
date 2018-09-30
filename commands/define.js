ee7f9009288c5f3dba6b6267cfee950c3197746b
bigwoke <djk152@gmail.com>
2018-09-30 02:48:48 -0400

    refine define, add urban.js

--- commands/define.js
+++ commands/define.js
@@ -18,14 +18,18 @@ module.exports.run = async (ts, ev, client, args) => {
             let response = JSON.parse(data);
             let definitions = response.length;
 
-            let msg = `There are ${definitions} definitions for the word "${word}":\n`;
+            let msg = `Found ${definitions} definition(s) for the word "${word}":\n`;
 
             for(let ct = 0; ct < definitions && msg.length < 900; ct++) {
                 let type = response[ct].type;
                 let def = response[ct].definition.replace(/â/g, '"');
-                msg += `\t[b]${ct + 1}[/b]:  ${type}. ${def}\n`;
-            }
+                let append = `\t[b]${ct + 1}[/b]:  ${type}. ${def}\n`;
 
+                if((msg + append).length >= 1024) {
+                    break;
+                }
+                msg += append;
+            }
             ts.sendTextMessage(client.getID(), ev.targetmode, msg);
         });
     });
---
+++ commands/urban.js
@@ -0,0 +1,41 @@
+const https = require('https');
+
+module.exports.run = async (ts, ev, client, args) => {
+    let phrase = args.join(' ');
+    if(!phrase) return ts.sendTextMessage(client.getID(), 1, 'error: Missing argument(s)!');
+
+    let dict_url = 'https://api.urbandictionary.com/v0/define?term=' + encodeURIComponent(phrase);
+    https.get(dict_url, resp => {
+        let data = '';
+
+        resp.on('data', chunk => {
+            data += chunk;
+        });
+
+        resp.on('end', () => {
+            let response = JSON.parse(data).list;
+            let entries = response.length >= 3 ? 3 : response.length;
+            let msg = `Here are the top ${entries} entries on Urban Dictionary for "${phrase}":\n`;
+
+            for(let ct = 0; ct < entries; ct++) {
+                let def = response[ct].definition.replace(/[[\]]/g, '').replace(/\n+/g, ' ');
+                let votes = response[ct].thumbs_up;
+                let append = `\t[b]${ct + 1}[/b]:  ${def}\t[i](${votes} upvotes)[/i]\n`;
+                
+                if((msg + append).length >= 1024) {
+                    msg = msg.replace(entries, ct);
+                    break;
+                }
+                msg += append;
+            }
+            ts.sendTextMessage(client.getID(), ev.targetmode, msg);
+        });
+    });
+};
+
+module.exports.info = {
+    name: 'urban',
+    usage: `${process.env.PREFIX}urban <word>`,
+    desc: 'Grabs the definition(s) for the given word from Urban Dictionary.',
+    level: 2
+};
