030425446680009d1f7601e768afcde2823b6bda
bigwoke <djk152@gmail.com>
2019-03-31 17:03:29 -0400

    implement uidadd, replace userclear with uiddel

---
+++ commands/db/uidadd.js
@@ -0,0 +1,40 @@
+const log = require('../../log.js')
+
+module.exports.run = async (ts, ev, client, args) => {
+  if (!args[1]) return ts.sendTextMessage(client.getID(), 1, 'error: Missing argument(s)!')
+
+  let match = await ts.data.collection('users').findOne({ name: args[0] })
+  if (!match) return ts.sendTextMessage(client.getID(), 1, 'Could not find user.')
+
+  let uidRegex = /^.{27}=$/g
+
+  if (!uidRegex.test(args[1])) {
+    return ts.sendTextMessage(client.getID(), 1, 'error: Unique ID does not match the required pattern.')
+  }
+
+  let filter = { name: args[0] }
+  let update = { $addToSet: { uid: args[1] } }
+
+  ts.data.collection('users').updateOne(filter, update, (err, res) => {
+    if (err) log.error('[DB] Error updating document:', err.stack)
+
+    if (res.result.n === 0) {
+      ts.sendTextMessage(client.getID(), 1, 'Couldn\'t find document, please report this bug.')
+    } else if (res.result.n === 1 && res.result.nModified === 0 && res.result.ok === 1) {
+      ts.sendTextMessage(client.getID(), 1, 'User document already has that ID.')
+    } else if (res.result.nModified === 1) {
+      ts.sendTextMessage(client.getID(), 1, 'Successfully added unique ID.')
+      log.info('[DB] Unique ID', args[1], 'added to', args[0])
+    } else {
+      ts.sendTextMessage(client.getID(), 1, 'Issue adding unique ID.')
+    }
+  })
+}
+
+module.exports.info = {
+  name: 'uidadd',
+  usage: `${process.env.PREFIX}uidadd <name> <unique ID>`,
+  desc: 'Adds a unique ID to a user document.',
+  module: 'db',
+  level: 0
+}
---
+++ commands/db/uiddel.js
@@ -0,0 +1,36 @@
+const log = require('../../log.js')
+
+module.exports.run = async (ts, ev, client, args) => {
+  if (!args[1]) return ts.sendTextMessage(client.getID(), 1, 'error: Missing argument(s)!')
+
+  let filter = { name: args[0] }
+  let update = { $pull: { uid: args[1] } }
+  if (args[1] === '*') {
+    update = { $set: { uid: [] } }
+  }
+
+  ts.data.collection('users').updateOne(filter, update, (err, res) => {
+    if (err) log.error('Error removing unique ID:', err.stack)
+
+    console.log(args[1])
+
+    if (res.result.n === 0) {
+      ts.sendTextMessage(client.getID(), 1, 'Couldn\'t find document, please report this bug.')
+    } else if (res.result.n === 1 && res.result.nModified === 0 && res.result.ok === 1) {
+      ts.sendTextMessage(client.getID(), 1, 'User document does not have that ID.')
+    } else if (res.result.nModified === 1) {
+      ts.sendTextMessage(client.getID(), 1, 'Successfully removed unique ID.')
+      log.info('[DB] Unique ID', args[1], 'removed from', args[0])
+    } else {
+      ts.sendTextMessage(client.getID(), 1, 'Issue removing unique ID.')
+    }
+  })
+}
+
+module.exports.info = {
+  name: 'uiddel',
+  usage: `${process.env.PREFIX}uiddel <name> <unique ID>`,
+  desc: 'Removes a unique ID from a user document.',
+  module: 'db',
+  level: 0
+}
