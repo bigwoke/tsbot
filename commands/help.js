09e369405c9796864f3d840ff28a16cbe16e2a0a
bigwoke <djk152@gmail.com>
2019-06-05 02:01:21 -0400

    can now see help only for a specified command

--- commands/help.js
+++ commands/help.js
@@ -1,40 +1,52 @@
 const log = require('../log.js')
 
-module.exports.run = async (ts, ev, client) => {
-  let count = 1
-  let resp = `[U]Command Page ${count}[/U]:\n`
-
-  let commands = new Map(Array
-    .from(ts.commands)
-    .sort((a, b) => {
-      if (a[0] < b[0]) return -1
-      if (a[0] > b[0]) return 1
-      return 0
-    }))
-
-  commands.forEach(cmd => {
+module.exports.run = async (ts, ev, client, args) => {
+  if (args[0]) {
+    let cmd = ts.commands.get(args[0])
+    if (!cmd) return ts.sendTextMessage(client.getID(), 1, `No command with the name "${args[0]}" exists.`)
     let color = (cmd.info.level === 2) ? '#00825a' : (cmd.info.level === 1) ? '#d58500' : '#ff3300'
-    if (client.level <= cmd.info.level) {
-      if (resp.length >= ts.charLimit - 100) {
-        ts.sendTextMessage(client.getID(), 1, resp).catch(err => {
-          ts.sendTextMessage(client.getID(), 1, 'error: Too many characters in response.')
-          log.error(err)
-        })
-        count++
-        resp = `[U]Command Page ${count}[/U]:\n`
-      }
-      resp += `[color=${color}]${cmd.info.usage}[/color] - ${cmd.info.desc}\n`
-    }
-  })
 
-  ts.sendTextMessage(client.getID(), 1, resp).catch(err => {
-    if (err.id === 1541) {
-      ts.sendTextMessage(client.getID(), 1, 'error: Too many characters, please report this bug.')
-      log.error(err)
+    if (client.level <= cmd.info.level) {
+      ts.sendTextMessage(client.getID(), 1, `[color=${color}]${cmd.info.usage}[/color] - ${cmd.info.desc}\n`)
     } else {
-      log.error(err)
+      ts.sendTextMessage(client.getID(), 1, 'You do not have permission to view info for this command.')
     }
-  })
+  } else {
+    let count = 1
+    let resp = `[U]Command Page ${count}[/U]:\n`
+
+    let commands = new Map(Array
+      .from(ts.commands)
+      .sort((a, b) => {
+        if (a[0] < b[0]) return -1
+        if (a[0] > b[0]) return 1
+        return 0
+      }))
+
+    commands.forEach(cmd => {
+      let color = (cmd.info.level === 2) ? '#00825a' : (cmd.info.level === 1) ? '#d58500' : '#ff3300'
+      if (client.level <= cmd.info.level) {
+        if (resp.length >= ts.charLimit - 100) {
+          ts.sendTextMessage(client.getID(), 1, resp).catch(err => {
+            ts.sendTextMessage(client.getID(), 1, 'error: Too many characters in response.')
+            log.error(err)
+          })
+          count++
+          resp = `[U]Command Page ${count}[/U]:\n`
+        }
+        resp += `[color=${color}]${cmd.info.usage}[/color] - ${cmd.info.desc}\n`
+      }
+    })
+
+    ts.sendTextMessage(client.getID(), 1, resp).catch(err => {
+      if (err.id === 1541) {
+        ts.sendTextMessage(client.getID(), 1, 'error: Too many characters, please report this bug.')
+        log.error(err)
+      } else {
+        log.error(err)
+      }
+    })
+  }
 }
 
 module.exports.info = {
