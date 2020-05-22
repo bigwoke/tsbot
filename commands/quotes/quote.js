33bdd6d741935e4abe48835dbd57829044bdf84a
bigwoke <djk152@gmail.com>
2020-05-21 22:34:51 -0400

    Add timeouts for individual quotes, preventing repeats in display

--- commands/quotes/quote.js
+++ commands/quotes/quote.js
@@ -1,5 +1,7 @@
 const log = require('../../log.js')
 
+const timeouts = new Map()
+
 module.exports.run = async (ts, ev, client, args) => {
   if (!args[0]) {
     getRandomQuote((quote, author) => {
@@ -54,17 +56,35 @@ module.exports.run = async (ts, ev, client, args) => {
   }
 
   function getRandomQuote (callback) {
-    ts.data.collection('quotes').aggregate([{ $sample: { size: 1 } }]).toArray((err, res) => {
-      if (err) log.error('[DB] Error getting quote from database:', err.stack)
-      let quote = res[0]
+    ts.data.collection('quotes').estimatedDocumentCount().then(ct => {
+      if (timeouts.size === ct - 1) {
+        timeouts.delete(Array.from(timeouts.keys())[0])
+      }
 
-      ts.data.collection('users').findOne({ _id: quote.author }, (error, result) => {
-        if (error) log.error('[DB] Error getting quote from database:', error.stack)
-        let author = result
-        if (!result) {
-          author = { name: quote.author }
+      const pipeline = [
+        { $match: { number: { $nin: Array.from(timeouts.keys()) } } },
+        { $sample: { size: 1 } }
+      ]
+
+      ts.data.collection('quotes').aggregate(pipeline).toArray((err, res) => {
+        if (err) log.error('[DB] Error getting quote from database:', err.stack)
+        let quote = res[0]
+  
+        if (quote) {
+          const quoteNumber = quote.number
+          const timeoutFunc = () => timeouts.delete(quoteNumber)
+  
+          timeouts.set(quoteNumber, { author: author, timeout: setTimeout(timeoutFunc, ct * 5000) })
         }
-        callback(quote, author)
+  
+        ts.data.collection('users').findOne({ _id: quote.author }, (error, result) => {
+          if (error) log.error('[DB] Error getting quote from database:', error.stack)
+          let author = result
+          if (!result) {
+            author = { name: quote.author }
+          }
+          callback(quote, author)
+        })
       })
     })
   }
@@ -93,28 +113,55 @@ module.exports.run = async (ts, ev, client, args) => {
       ts.data.collection('users').findOne({ name: searchTerm }, (err, res) => {
         if (err) log.error('[DB] Error getting quote from database:', err.stack)
         if (!res) {
-          ts.data.collection('quotes').find({ author: searchTerm }).toArray((error, result) => {
-            if (error) log.error('[DB] Error getting quote from database:', error.stack)
-            if (result.length === 0) {
-              return ts.sendTextMessage(client.getID(), 1, 'Could not find quote by that user.')
-            }
-            let author = { name: searchTerm }
-            let quote = result[Math.floor(result.length * Math.random())]
-            callback(quote, author)
-          })
-        } else {
-          let author = res
-
-          ts.data.collection('quotes').find({ author: author._id }).toArray((error, result) => {
-            if (error) log.error('[DB] Error getting quote from database:', error.stack)
-            if (result.length === 0) {
-              return ts.sendTextMessage(client.getID(), 1, 'Could not find quote by that user.')
-            }
-
-            let quote = result[Math.floor(result.length * Math.random())]
-            callback(quote, author)
-          })
+          return ts.sendTextMessage(client.getID(), 1, 'Could not find that user.')
         }
+
+        let author = res
+
+        const match = { author: author._id }
+
+        const pipeline = [
+          { $match: { $and: [match, { number: { $nin: Array.from(timeouts.keys()) } }] } },
+          { $sample: { size: 1 } }
+        ]
+
+        ts.data.collection('quotes').countDocuments(match).then(async ct => {
+          const timeoutVals = Array.from(timeouts.values())
+          const timeoutsMatchingUser = timeoutVals.filter(v => v.author._id.equals(author._id))
+          
+          console.log(timeoutsMatchingUser.length, ct)
+          if (timeoutsMatchingUser.length === ct - 1) {
+            (() => {
+              for (const to of timeouts) {
+                let key = to[0]
+                let val = to[1]
+  
+                if (val.author._id.equals(author._id)) {
+                  console.log(key)
+                  return timeouts.delete(key)
+                }
+              }
+            })() // IIFE IS VERY UGLY BUT WORKS
+          }
+
+          ts.data.collection('quotes').aggregate(pipeline).toArray((error, result) => {
+            if (error) log.error('[DB] Error getting quote from database:', error.stack)
+            if (!result[0]) {
+              return ts.sendTextMessage(client.getID(), 1, 'Could not find quote by that user.')
+            }
+
+            const quote = result[0]
+            
+            if (quote) {
+              const quoteNumber = quote.number
+              const timeoutFunc = () => timeouts.delete(quoteNumber)
+      
+              timeouts.set(quoteNumber, { author: author, timeout: setTimeout(timeoutFunc, ct * 5000) })
+            }
+
+            callback(quote, author);
+          })
+        })          
       })
     }
   }
